2d7a05c49c3a60d48e3f51c604db4511f1deff87
fanzheng.fz <fanzheng.fz@alibaba-inc.com>
2016-08-01 23:28:04 +0800

    set dry mode as default

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 import Runner from 'jscodeshift/dist/Runner';
 import path from 'path';
 
-export default function parse({ sourcePath }) {
+export default function parse({ sourcePath, options }) {
   Runner.run(
     path.resolve('./src/transform.js'),
     [sourcePath],
     {
       extensions: 'js,jsx',
+      dry: true,
+      ...options,
     },
   );
 }
