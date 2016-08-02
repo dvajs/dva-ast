5aaa13e09810c25acf85adb1fdb53af87f047f22
fanzheng.fz <fanzheng.fz@alibaba-inc.com>
2016-08-02 22:26:20 +0800

    add es6class & createClass components demo

---
+++ demo/user-dashboard/src/components/tests/Agent.jsx
@@ -0,0 +1,11 @@
+import React, { PropTypes } from 'react'
+
+class Agent extends React.Component {
+  render() {
+    return (
+      <div>Agent</div>
+    );
+  }
+}
+
+export default Agent;
---
+++ demo/user-dashboard/src/components/tests/Customer.jsx
@@ -0,0 +1,14 @@
+var React = require('react');
+var PropTypes = React.PropTypes;
+
+var Customer = React.createClass({
+
+  render: function() {
+    return (
+      <div />
+    );
+  }
+
+});
+
+module.exports = Customer;
