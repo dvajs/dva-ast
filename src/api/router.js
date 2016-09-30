78cab1e71312fdf31aca98f34145c9a16e9894ee
fanzheng.fz <fanzheng.fz@alibaba-inc.com>
2016-09-30 17:58:52 +0800

    feat: add api, router.createRoute createIndexRedirect createRedirect

--- src/api/router.js
+++ src/api/router.js
@@ -1,3 +1,152 @@
+import {
+  getTemplate,
+  writeFile,
+  readFile,
+  removeFile,
+} from './utils';
+import { existsSync } from 'fs';
+import { getExpression } from '../utils/index';
+import { join, relative, sep } from 'path';
+import assert from 'assert';
+import j from 'jscodeshift';
 
-export function create(payload) {}
+// TODO: check react-router version
+// assume that router.js is already created
+function findParentRoute(root) {
+  // TODO, support find by parentId
+  return root.find(
+    j.JSXElement, {
+      openingElement: {
+        name: {
+          name: 'Router'
+        }
+      }
+    }
+  ).nodes()[0];
+}
 
+function createElement(root, el, attributes = []) {
+  const parentRoute = findParentRoute(root);
+  parentRoute.children.push(
+    j.jsxElement(
+      j.jsxOpeningElement(
+        j.jsxIdentifier(el),
+        attributes.map(attr => {
+          if (attr.isExpression) {
+            return j.jsxAttribute(
+              j.jsxIdentifier(attr.key),
+              j.jsxExpressionContainer(
+                j.identifier(attr.value)
+              )
+            )
+          } else {
+            return j.jsxAttribute(
+              j.jsxIdentifier(attr.key),
+              j.literal(attr.value)
+            );
+          }
+        })
+      ),
+      j.jsxClosingElement(
+        j.jsxIdentifier(el)
+      ),
+      []
+    )
+  );
+  parentRoute.children.push(j.jsxText('\n'));
+}
+
+export function createRoute(payload) {
+  const { path, component = {} } = payload;
+  assert(
+    path && component.componentName,
+    'api/router/createRoute: payload should at least have path or compnent'
+  );
+
+  const filePath = join(payload.sourcePath, payload.filePath);
+  const source = readFile(filePath);
+  const root = j(source);
+  const parentRoute = findParentRoute(root);
+
+  // append Route
+  // TODO: support additonal attributes like components
+  const attributes = [];
+  if (path) {
+    attributes.push({ key: 'path', value: path });
+  }
+  if (component.componentName) {
+    attributes.push({ key: 'component', value: component.componentName, isExpression: true });
+  }
+  createElement(root, 'Route', attributes);
+
+  if (!component.componentName) return writeFile(filePath, root.toSource());
+  assert(
+    component.filePath,
+    'api/router/create: payload.component should have filePath'
+  );
+
+  // create & import component
+  let relativePath;
+  const componentFilePath = join(payload.sourcePath, component.filePath);
+  if (existsSync(componentFilePath)) {
+    relativePath = relative(filePath, componentFilePath);
+    relativePath = relativePath.split(sep).join('/');  // workaround for windows
+  }
+
+  const imports = root.find(j.ImportDeclaration);
+  const lastImport = imports.at(imports.size() - 1);
+  lastImport.insertAfter(
+    j.importDeclaration(
+      [j.importDefaultSpecifier(
+        j.identifier(component.componentName)
+      )],
+      j.literal(relativePath)
+    )
+  );
+  writeFile(filePath, root.toSource());
+}
+
+export function createIndexRoute() {
+
+}
+
+export function createRedirect(payload) {
+  assert(
+    payload.from && payload.to,
+    'api/router/createRedirect: payload should have from or to'
+  );
+  const filePath = join(payload.sourcePath, payload.filePath);
+  const source = readFile(filePath);
+  const root = j(source);
+
+  createElement(
+    root,
+    'Redirect',
+    [
+      { key: 'from', value: payload.from },
+      { key: 'to', value: payload.to },
+    ]
+  );
+
+  writeFile(filePath, root.toSource());
+}
+
+export function createIndexRedirect(payload) {
+  assert(
+    payload.to,
+    'api/router/createIndexRedirect: payload should have to'
+  );
+  const filePath = join(payload.sourcePath, payload.filePath);
+  const source = readFile(filePath);
+  const root = j(source);
+
+  createElement(
+    root,
+    'IndexRedirect',
+    [
+      { key: 'to', value: payload.to },
+    ]
+  );
+
+  writeFile(filePath, root.toSource());
+}
---
+++ test/api/router-test.js
@@ -0,0 +1,88 @@
+import api from '../../src/api/index';
+import expect from 'expect';
+import { removeSync, outputFileSync } from 'fs-extra';
+import { join, dirname } from 'path';
+
+describe('api/routeComponents', () => {
+  const filePath = './tmp/router.js';
+  const absFilePath = join(__dirname, filePath);
+
+  afterEach(() => {
+    // removeSync(dirname(absFilePath));
+  });
+
+  const prepareRouterjs = () => {
+    outputFileSync(absFilePath,
+`import React, { PropTypes } from 'react';
+import { Router, Route, IndexRoute, Link } from 'dva/router';
+import IndexPage from './routes/IndexPage';
+import UserPage from './routes/UserPage';
+import UserDetailPage from './routes/UserDetailPage';
+
+export default function({ history }) {
+  return (
+    <Router history={history}>
+      <Route path="/" component={IndexPage} />
+      <Route path="/users" component={UserPage} >
+        <Route path="user" component={UserDetailPage} />
+      </Route>
+    </Router>
+  );
+};`);
+  };
+
+  const componentFilePath = './tmp/ccccc/Test.jsx';
+  const prepareComponent = () => {
+    api('routeComponents.create', {
+      filePath: componentFilePath,
+      sourcePath: __dirname,
+      componentName: 'Test',
+    });
+  }
+
+  it('router.createRoute: without parent', () => {
+    prepareRouterjs();
+    prepareComponent();
+
+    const result = api('router.createRoute', {
+      filePath,
+      sourcePath: __dirname,
+      path: 'Test',
+      component: {
+        componentName: 'Test',
+        filePath: componentFilePath,
+      },
+    });
+    expect(result).toEqual({"models":{"data":[],"reducerByIds":{},"effectByIds":{},"subscriptionByIds":{}},"router":{"type":"Router","attributes":{"history":"history"},"id":"Router-root","children":[{"type":"Route","attributes":{"path":"/","component":"IndexPage"},"absolutePath":"/","id":"Route-/","children":[]},{"type":"Route","attributes":{"path":"/users","component":"UserPage"},"absolutePath":"/users","id":"Route-/users","children":[{"type":"Route","attributes":{"path":"user","component":"UserDetailPage"},"absolutePath":"/users/user","id":"Route-/users/user","children":[]}]},{"type":"Route","attributes":{"path":"Test","component":"Test"},"absolutePath":"/Test","id":"Route-/Test","children":[]}],"filePath":"./tmp/router.js"},"routeComponents":[],"dispatches":{}});
+  });
+
+  // it('router.createRoute: with parent', () => {
+  //   const result = {i: 1};
+  //   expect(result).toEqual({})
+  // });
+
+  it('router.createRedirect', () => {
+    prepareRouterjs();
+
+    const result = api('router.createRedirect', {
+      filePath,
+      sourcePath: __dirname,
+      from: '/a/b',
+      to: '/a/c',
+    });
+
+    expect(result).toEqual({"models":{"data":[],"reducerByIds":{},"effectByIds":{},"subscriptionByIds":{}},"router":{"type":"Router","attributes":{"history":"history"},"id":"Router-root","children":[{"type":"Route","attributes":{"path":"/","component":"IndexPage"},"absolutePath":"/","id":"Route-/","children":[]},{"type":"Route","attributes":{"path":"/users","component":"UserPage"},"absolutePath":"/users","id":"Route-/users","children":[{"type":"Route","attributes":{"path":"user","component":"UserDetailPage"},"absolutePath":"/users/user","id":"Route-/users/user","children":[]}]},{"type":"Redirect","attributes":{"from":"/a/b","to":"/a/c"},"id":"Redirect-parentId_Router-root","children":[]}],"filePath":"./tmp/router.js"},"routeComponents":[],"dispatches":{}});
+  });
+
+  it('router.createIndexRedirect', () => {
+    prepareRouterjs();
+
+    const result = api('router.createIndexRedirect', {
+      filePath,
+      sourcePath: __dirname,
+      to: '/a/c',
+    });
+
+    expect(result).toEqual({"models":{"data":[],"reducerByIds":{},"effectByIds":{},"subscriptionByIds":{}},"router":{"type":"Router","attributes":{"history":"history"},"id":"Router-root","children":[{"type":"Route","attributes":{"path":"/","component":"IndexPage"},"absolutePath":"/","id":"Route-/","children":[]},{"type":"Route","attributes":{"path":"/users","component":"UserPage"},"absolutePath":"/users","id":"Route-/users","children":[{"type":"Route","attributes":{"path":"user","component":"UserDetailPage"},"absolutePath":"/users/user","id":"Route-/users/user","children":[]}]},{"type":"IndexRedirect","attributes":{"to":"/a/c"},"id":"IndexRedirect-parentId_Router-root","children":[]}],"filePath":"./tmp/router.js"},"routeComponents":[],"dispatches":{}});
+  });
+});
